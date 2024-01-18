cf641dae719a9863358845dbb6d290c3e7fa1425
Evgeniy <sevg3ny@gmail.com>
2024-01-18 10:28:40 +0300

    user add form send to back

--- assets/js/admin/user.js
+++ assets/js/admin/user.js
@@ -1,22 +1,29 @@
 function createUser() {
     // создать объект для формы
     let formData = new FormData(document.forms.userCreateForm);
-    formData.append('departments', [document.getElementById('departments').value]);
-    formData.append('specializations', [document.getElementById('specializations').value]);
-    formData.append('positions',[document.getElementById('positions').value]);
     let jsonObj = {}
     formData.forEach((value, key) => {
         jsonObj[key] = value;
     });
+    jsonObj['departments'] = [document.getElementById('departments').value]
+    jsonObj['specializations'] = [document.getElementById('specializations').value]
+    jsonObj['positions'] = [document.getElementById('positions').value]
     let xhr = new XMLHttpRequest();
     xhr.open("POST", "/api/user/");
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.onreadystatechange = function() {
         if (this.readyState === 4) {
             if (this.status === 200) {
-                console.log("good");
-                // window.location.href = JSON.parse(xhr.responseText);
-            } else {
+                // Находим Toast и инициализируем его
+                var successToast = new bootstrap.Toast(document.getElementById('successToast'));
+
+                // Вызываем метод show() для отображения уведомления
+                successToast.show();
+
+                // Через 5 секунд скрываем уведомление (можете настроить время по своему усмотрению)
+                setTimeout(function() {
+                    successToast.hide();
+                }, 5000);            } else {
                 console.log("bad");
                 // document.getElementById('passwordInput').classList.add('is-invalid');
             }
